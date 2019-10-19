1e2724c55ed51ea81316f5cb6e923760dfbc3581
Juan Ortiz <ortizub41@gmail.com>
2019-10-19 14:16:53 -0400

    Added routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import { Container, Grid, Row, Col } from 'react-bootstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
@@ -13,18 +19,17 @@ function App() {
           rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to exoPlanIt
         </p>
       </header>
-      <body>
         <Container>
           <Row className="justify-content-md-center custom-row-spacing">
             <Col xs lg="2">
-              <Button variant="primary" className ="full-width-btn">Reset</Button>
+              <Link to="/create"><Button variant="primary" className ="full-width-btn">Reset</Button></Link>
             </Col>
             <Col xs lg="2">
               <Button variant="primary" className ="full-width-btn">Create</Button>
@@ -36,9 +41,32 @@ function App() {
             </Col>
           </Row>
         </Container>
-      </body>
     </div>
   );
 }
 
-export default App;
+function Routing() {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <App />
+                </Route>
+                <Route path="/create">
+                    <Create />
+                </Route>
+            </Switch>
+        </Router>
+    )
+}
+
+function Create() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <Link to="/"><Button variant="primary" className ="full-width-btn">Reset</Button></Link>
+        </div>
+    );
+}
+
+export default Routing;
