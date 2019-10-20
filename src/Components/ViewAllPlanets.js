c1c209f41b63860239688c1a03b41e7c41f545f3
Immanuel George <immanuel.george@vertical-knowledge.com>
2019-10-20 15:24:09 -0400

    redirect viel all planets

--- src/Components/ViewAllPlanets.js
+++ src/Components/ViewAllPlanets.js
@@ -1,1 +1,12 @@
- 
+import React, { Component } from 'react';
+
+class ViewAllPlanets extends Component {
+
+  render (){
+    return (
+      window.location.href = "https://exo-planet-starflock-backend.herokuapp.com/get_all_users"
+  );
+}
+}
+
+export default ViewAllPlanets;
